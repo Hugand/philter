4d5c674ad693904d15e342e52e51d9cf267da26d
Hugo Gomes <hugoasgomes@hotmail.com>
2020-10-20 13:46:29 +0100

    Implement histogram

--- philter/src/components/blocks/edit-photo-preview.js
+++ philter/src/components/blocks/edit-photo-preview.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../styles/blocks/edit-photo-preview.scss';
 import Canvas from '../atoms/canvas';
 import { useStateValue } from  '../../state'
+import HistogramComponent from '../atoms/histogram';
 
 function EditPhotoPreview(props) {
   const [ { imageData, image } ] = useStateValue()
@@ -17,8 +18,11 @@ function EditPhotoPreview(props) {
   return (
     <section className={ props.class }>
       <header>
-          <button className="btn-secondary" onClick={props.goBack}>back</button>
-          <button className="btn-primary" onClick={saveImage}>Save as</button>
+        <button className="btn-secondary" onClick={props.goBack}>back</button>
+        <button className="btn-primary" onClick={saveImage}>Save as</button>
+
+        <HistogramComponent></HistogramComponent>
+
       </header>
 
       {
